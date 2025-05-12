f519812346916a266d90eff0d9c5ace5a0bfd48e
willbezero <hsy6927@naver.com>
2025-05-12 17:27:59 +0900

    Add files via upload

--- mystreamlit.py
+++ mystreamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("환영합니다")
-st.markdown("효정이는 내꺼")
-
-st.video('https://www.youtube.com/watch?v=KaVC81mtbS4')
+st.title("✨ 환영합니다, 효정님! ✨")
+st.markdown("🔥 오늘도 예쁘시네요요 🔥")
+st.video("https://www.youtube.com/watch?v=KaVC81mtbS4")
+st.write("🚀 곧 마친다!")
